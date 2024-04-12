8db8387c8ad19e0a25f787f4a8bdbfca79dc9dd2
Jose Carlos Canova <110000@CP-DK-148092.intranet.ufscar.br>
2024-04-12 05:40:29 -0300

    Update service for collection Creation.;

--- schema-metaclass/src/main/java/org/nanotek/meta/rdbms/service/RdbmsMetaClassService.java
+++ schema-metaclass/src/main/java/org/nanotek/meta/rdbms/service/RdbmsMetaClassService.java
@@ -1,6 +1,11 @@
 package org.nanotek.meta.rdbms.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.sql.DataSource;
 
 import org.nanotek.meta.model.MetaClass;
 import org.nanotek.meta.model.MetaDataAttribute;
@@ -9,7 +14,10 @@ import org.nanotek.meta.util.ColumnNameTranslationStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import schemacrawler.schema.Catalog;
 import schemacrawler.schema.Column;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.tools.utility.SchemaCrawlerUtility;
 
 
 
@@ -18,6 +26,25 @@ public class RdbmsMetaClassService {
 
 	@Autowired
 	ColumnNameTranslationStrategy columnNameTranslationStrategy; 
+	
+	@Autowired
+	DataSource defaultDataSource;
+	
+	@Autowired 
+	SchemaCrawlerOptions schemaCrawlerOptions;
+	
+	public List<schemacrawler.schema.Table> getSchemaTables(){
+		Catalog  catalog;
+		List<schemacrawler.schema.Table> tables = new ArrayList<schemacrawler.schema.Table>();
+		try {
+			Connection connection = defaultDataSource.getConnection();
+			catalog = SchemaCrawlerUtility.getCatalog(connection, schemaCrawlerOptions);
+			catalog.getTables().forEach(t ->tables.add(t));;
+		} catch (Exception e) {
+			throw new SchemaMetaClassException(e);
+		}
+		return tables;
+	}
 	
 	public MetaClass createMetaClass(schemacrawler.schema.Table t) {
 		MetaClass meta = new MetaClass();
---
+++ schema-metaclass/src/main/java/org/nanotek/meta/rdbms/service/SchemaMetaClassException.java
@@ -0,0 +1,27 @@
+package org.nanotek.meta.rdbms.service;
+
+public class SchemaMetaClassException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public SchemaMetaClassException() {
+	}
+
+	public SchemaMetaClassException(String message) {
+		super(message);
+	}
+
+	public SchemaMetaClassException(Throwable cause) {
+		super(cause);
+	}
+
+	public SchemaMetaClassException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public SchemaMetaClassException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
