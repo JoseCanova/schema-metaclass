f6c656295f840310f88258cc88697e480cb4476e
Jose Carlos Canova <110000@CP-DK-148092.intranet.ufscar.br>
2024-04-19 02:28:47 -0300

    Added top interface for Controller for MetaClass Hierarchy.

---
+++ schema-metaclass/src/main/java/org/nanotek/meta/controller/RepositoryController.java
@@ -0,0 +1,8 @@
+package org.nanotek.meta.controller;
+
+import org.nanotek.meta.model.MetaClass;
+import org.nanotek.meta.repository.MetaClassRepository;
+
+public interface RepositoryController<T extends MetaClassRepository<B>, B extends MetaClass<B>> {
+	T getRepository();
+}
