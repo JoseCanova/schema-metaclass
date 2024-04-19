0ae5e01e879ad92929ecb5fab37c7ef2d6c8e09a
Jose Carlos Canova <110000@CP-DK-148092.intranet.ufscar.br>
2024-04-19 03:43:37 -0300

    Controller Model Update

---
+++ schema-metaclass/src/main/java/org/nanotek/meta/controller/MetaClassController.java
@@ -0,0 +1,9 @@
+package org.nanotek.meta.controller;
+
+import org.nanotek.meta.model.MetaClass;
+import org.nanotek.meta.repository.MetaClassRepository;
+
+public interface MetaClassController<T extends MetaClassRepository<B>, B extends MetaClass<B>>
+extends RepositoryController<T , B> 
+{
+}
