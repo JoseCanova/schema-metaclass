d23ce9b6f445bbf981d9d2d44680cb7bf7a93398
Jose Carlos Canova <110000@CP-DK-148092.intranet.ufscar.br>
2024-04-21 09:17:00 -0300

    Polishing

--- schema-metaclass/src/main/java/org/nanotek/meta/rdbms/exception/SchemaMetaClassValidationException.java
+++ schema-metaclass/src/main/java/org/nanotek/meta/rdbms/exception/SchemaMetaClassValidationException.java
@@ -4,9 +4,6 @@ import org.springframework.core.NestedRuntimeException;
 
 public class SchemaMetaClassValidationException extends NestedRuntimeException {
     
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 3352027202324570248L;
 
 	public SchemaMetaClassValidationException(String message) {
