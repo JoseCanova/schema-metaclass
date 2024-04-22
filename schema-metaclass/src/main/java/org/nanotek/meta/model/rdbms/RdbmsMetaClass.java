7041bdffe6c0189ab0a8c2aba458b9e28ca0de20
Jose Carlos Canova <110000@CP-DK-148092.intranet.ufscar.br>
2024-04-22 02:02:32 -0300

    Polishing

--- schema-metaclass/src/main/java/org/nanotek/meta/model/rdbms/RdbmsMetaClass.java
+++ schema-metaclass/src/main/java/org/nanotek/meta/model/rdbms/RdbmsMetaClass.java
@@ -6,10 +6,13 @@ import org.nanotek.meta.model.IRdbmsClass;
 import org.nanotek.meta.model.MetaClass;
 import org.nanotek.meta.model.MetaClassAttribute;
 import org.nanotek.meta.model.MetaClassClassifier;
+import org.nanotek.meta.validation.MetaClassDefaultValidationGroup;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import schemacrawler.schema.Table;
 
 //TODO: move attributes relative to rdbms here. 
@@ -17,10 +20,12 @@ public class RdbmsMetaClass extends MetaClass<RdbmsMetaClass> implements IRdbmsC
 
 	private static final long serialVersionUID = -4542645486119141998L;
 
+	@NotEmpty(groups= {MetaClassDefaultValidationGroup.class})
 	@JsonProperty("tableName")
 	protected String tableName;
 	
 	@JsonIgnore
+	@NotNull(groups= {MetaClassDefaultValidationGroup.class})
 	protected RdbmsClass rdbmsClass;
 	
 	public RdbmsMetaClass() {
