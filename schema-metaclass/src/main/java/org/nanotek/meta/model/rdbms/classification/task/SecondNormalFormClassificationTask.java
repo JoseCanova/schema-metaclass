e49c453924694b01ed3a04713221ef19874ee546
José Carlos Canova <110000@CP-DK-148935.intranet.ufscar.br>
2024-05-04 23:21:01 -0300

    Generating first iterations for Second Normal Form.

--- schema-metaclass/src/main/java/org/nanotek/meta/model/rdbms/classification/task/SecondNormalFormClassificationTask.java
+++ schema-metaclass/src/main/java/org/nanotek/meta/model/rdbms/classification/task/SecondNormalFormClassificationTask.java
@@ -1,25 +1,68 @@
 package org.nanotek.meta.model.rdbms.classification.task;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.nanotek.meta.model.rdbms.classification.data.ClassificationData;
-import org.nanotek.meta.model.rdbms.classification.data.ClassificationDataPair;
 import org.nanotek.meta.model.rdbms.classification.data.ClassificationResult;
 import org.springframework.stereotype.Component;
+
+import com.google.common.collect.ImmutableList;
+
+import schemacrawler.schema.Column;
 
 
 //TODO: prepare the evaluations of the method for unit test.
 @Component
 public class SecondNormalFormClassificationTask implements TableClassificationTask<ClassificationData> {
-
 	
 	public SecondNormalFormClassificationTask() {
 	}
-
 	
+	//TODO:Finish the second iteration with indexes. 
+	//TODO: Generate bad case scenarios to refine algorithm.
 	@Override
 	public Optional<ClassificationResult<?>> evaluate(ClassificationData cd) {
+		
+			Optional<List<Column>> theTableColumns = cd.schemaTable().table().map(t -> t.getColumns());
+		
+		    Map<?,?> kcMap=	cd
+			.key()
+			.opkey()
+			.map(k ->{
+				ImmutableList<Column> ilc = ImmutableList.copyOf(k.getConstrainedColumns());
+				return ilc
+				.stream()
+				.map(c -> 
+					theTableColumns
+					.get()
+					.stream()
+					.filter(c1 ->c1.getName().equalsIgnoreCase(c.getName()))
+					.findFirst()
+					.map(cc-> getEntry(c,cc))
+				).collect(Collectors.toList())
+				.stream()
+					.map(ok->ok.get())
+					.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+			}).get();
+		    cd
+			    .tableIndexes()
+			    .indexes()
+			    .filter(ic -> ic.size() > 0)
+			    .get()
+			    .stream()
+			    .filter(idx -> idx.isUnique())
+			    .forEach(ui ->{
+			    	ImmutableList<Column> ilc = ImmutableList.copyOf(ui.getColumns());
+			    });
 		return TableClassificationTask.super.evaluate(cd);
+	}
+
+
+	private Map.Entry<?, ?> getEntry(Column c, Column cc) {
+		return Map.entry(c, cc);
 	}
 	
 }
