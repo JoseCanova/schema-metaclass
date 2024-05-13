718154739df13d65637ed05f7d5332d59f70c429
José Carlos Canova <110000@CP-DK-149539.intranet.ufscar.br>
2024-05-13 14:25:32 -0300

    Fixed return type of VoidTableClassificationTask

--- schema-metaclass/src/main/java/org/nanotek/meta/model/rdbms/classification/task/VoidTableClassificationTask.java
+++ schema-metaclass/src/main/java/org/nanotek/meta/model/rdbms/classification/task/VoidTableClassificationTask.java
@@ -3,14 +3,11 @@ package org.nanotek.meta.model.rdbms.classification.task;
 import java.util.Optional;
 
 import org.nanotek.meta.model.rdbms.classification.data.ClassificationData;
-import org.nanotek.meta.model.rdbms.classification.data.ClassificationResult;
-import org.nanotek.meta.model.rdbms.classification.data.TableTypeEnum;
 import org.nanotek.meta.model.rdbms.classification.data.result.VoidTableClassificationResult;
 import org.nanotek.meta.rdbms.service.SchemaCrawlerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import schemacrawler.schema.PrimaryKey;
 import schemacrawler.schema.Table;
 
 @Component
@@ -22,18 +19,18 @@ public class VoidTableClassificationTask implements TableClassificationTask<Clas
 	public VoidTableClassificationTask() {
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
-	public Optional<ClassificationResult<?>> evaluate(ClassificationData cd) {
+	public Optional<VoidTableClassificationResult> evaluate(ClassificationData cd) {
 			 return Optional.ofNullable(cd)
 					.filter(cda -> cda.tableColumns().columns().equals(Optional.empty()) 
 								|| cda.key().opkey().equals(Optional.empty()))
 					.map(cda -> buildClassificationResult(cda.schemaTable().table()))
-					.map(ocr -> ocr.get())
-					.map(cr ->  ClassificationResult.class.cast(cr));
+					.map(ocr -> ocr.get());
 	}
 
 	
-	private  Optional<ClassificationResult<?>> buildClassificationResult(Optional<Table> oTable) {
+	private  Optional<VoidTableClassificationResult> buildClassificationResult(Optional<Table> oTable) {
 		return oTable.map(t -> new VoidTableClassificationResult(t.getFullName()));
 	}
 
