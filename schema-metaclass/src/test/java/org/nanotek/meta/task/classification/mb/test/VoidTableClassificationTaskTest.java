546b9cee5118b00de01b23722050e71e9e3d33dc
José Carlos Canova <110000@CP-DK-149539.intranet.ufscar.br>
2024-05-18 10:17:36 -0300

    Changing a little test estrategy to load with mbcore data model...

--- schema-metaclass/src/test/java/org/nanotek/meta/task/classification/mb/test/VoidTableClassificationTaskTest.java
+++ schema-metaclass/src/test/java/org/nanotek/meta/task/classification/mb/test/VoidTableClassificationTaskTest.java
@@ -1,17 +1,14 @@
-package org.nanotek.meta.task.classification.test;
+package org.nanotek.meta.task.classification.mb.test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Collection;
 import java.util.Optional;
 
 import javax.sql.DataSource;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.nanotek.meta.model.rdbms.classification.data.TableTypeEnum;
 import org.nanotek.meta.model.rdbms.classification.task.VoidTableClassificationTask;
@@ -19,8 +16,10 @@ import org.nanotek.meta.rdbms.service.SchemaCrawlerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import schemacrawler.schema.Table;
 
-//@SpringBootTest
+
+@SpringBootTest
 public class VoidTableClassificationTaskTest {
 
 	@Autowired 
@@ -35,13 +34,15 @@ public class VoidTableClassificationTaskTest {
 	public VoidTableClassificationTaskTest() {
 	}
 
-//	@Test
+	@Test
 	void testVoidTableClassificationInjection() throws SQLException {
 		assertNotNull(voidTableClassificationTask);
 		assertNotNull(schemaCrawlerService);
-		schemaCrawlerService
-		.getCatalogTables()
+		Optional<Collection<Table>> oTables = schemaCrawlerService
+				.getCatalogTables();
+		oTables
 		.ifPresent(lt -> lt.forEach(t -> {
+			System.err.println("there are tables");
 			schemaCrawlerService
 			.getTableClassificationData(Optional.ofNullable(t))
 			.ifPresent(cd -> {
@@ -56,30 +57,7 @@ public class VoidTableClassificationTaskTest {
 					}} );
 			});
 		}));
-		assertTrue(1 == 1);
+		assertTrue(1 >= 1);
 	}
 	
-	@BeforeEach
-	private void prepareTest() throws SQLException {
-		assertNotNull(defaultDataSource);
-		Connection connection = defaultDataSource.getConnection();		
-        Statement stmt = connection.createStatement();
-        String sql = 
-        		"CREATE TABLE IF NOT EXISTS accounts (\r\n"
-        		+ ");"; 
-        stmt.executeUpdate(sql);
-        connection.close();
-	} 
-	
-	@AfterEach
-	private void closeTest() throws SQLException {
-		assertNotNull(defaultDataSource);
-		Connection connection = defaultDataSource.getConnection();		
-        Statement stmt = connection.createStatement();
-        String sql = 
-        		"DROP TABLE IF EXISTS accounts;"; 
-        stmt.executeUpdate(sql);
-        connection.close();		
-	}
-
 }
