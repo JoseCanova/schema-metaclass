086a373ae4940be8e2de55b9cec735233a926335
110000 <110000@CP-DK-149539.intranet.ufscar.br>
2024-08-05 12:31:17 -0300

    Updated Test for new strategy on rdbmsmetaclass creating.

--- schema-metaclass/src/test/java/org/nanotek/meta/rdbms/test/RdbmsMetaClassTest.java
+++ schema-metaclass/src/test/java/org/nanotek/meta/rdbms/test/RdbmsMetaClassTest.java
@@ -1,22 +1,33 @@
 package org.nanotek.meta.rdbms.test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.nanotek.meta.model.rdbms.RdbmsClass;
 import org.nanotek.meta.model.rdbms.RdbmsMetaClass;
+import org.nanotek.meta.model.rdbms.RdbmsMetaClassAttribute;
 import org.nanotek.meta.rdbms.service.SchemaCrawlerService;
+import org.nanotek.meta.util.ColumnNameTranslationStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import schemacrawler.schema.Column;
 import schemacrawler.schema.Table;
 
 
 @SpringBootTest
 public class RdbmsMetaClassTest {
+	
+	
+	@Autowired
+	ColumnNameTranslationStrategy columnNameTranslationStrategy; 
 	
 	@Autowired
 	SchemaCrawlerService schemaCrawlerService;
@@ -24,23 +35,49 @@ public class RdbmsMetaClassTest {
 	@Test
 	void testRdbmsMetaClass() {
 		assertNotNull(schemaCrawlerService);
-		getCatalogTables()
+		assertNotNull(columnNameTranslationStrategy);
+		Optional<Collection<Table>> ocTables = getCatalogTables();
+		
+		assertTrue(ocTables.isPresent());
+		
+		int tablesSize = ocTables.get().size();
+		
+		List<?> metaClassList = ocTables
 		.map(ct -> toStream(ct))
 		.orElseThrow()
-		.map(t -> toRdbmsMetaClass(t));
+		.map(t -> createMetaClass(t))
+		.collect(Collectors.toList());
+		
+		
+		assertTrue(tablesSize == metaClassList.size());
 	}
 	
 	
-	private RdbmsMetaClass toRdbmsMetaClass(Table table) {
+	private RdbmsMetaClass createMetaClass(Table table) {
 		String tableName = Optional.ofNullable(table.getFullName()).orElse(table.getName());
 		assertNotNull(tableName);
 		RdbmsMetaClass metaClass = new RdbmsMetaClass(tableName , tableName , table);
 		assertNotNull(metaClass.getTableName());
 		assertNotNull(metaClass.getRdbmsClass());
+		populateMetaClassAttributes(metaClass);
 		return metaClass;
 	}
 
 
+	
+	public void populateMetaClassAttributes(RdbmsMetaClass metaClass) {
+		final RdbmsMetaClass mc = metaClass;
+		RdbmsClass rc = mc.getRdbmsClass();
+		List<Column> lc = rc.getTable().getColumns();
+		
+		lc.forEach(c -> {
+			RdbmsMetaClassAttribute md = new RdbmsMetaClassAttribute();
+			md.setClazz(c.getColumnDataType().getTypeMappedClass());
+			md.setColumnName(c.getName());
+			String fieldName = columnNameTranslationStrategy.processNameTranslationStrategy(c.getName());
+			md.setFieldName(fieldName);
+		});
+	}
 	private Stream<Table> toStream(Collection<Table> ct) {
 		return ct.stream();
 	}
