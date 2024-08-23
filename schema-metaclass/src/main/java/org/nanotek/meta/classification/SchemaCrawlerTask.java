c8a2a2d1259285ae31e81bd215edbaf8c8be61ea
110000 <110000@CP-DK-149048.intranet.ufscar.br>
2024-08-23 13:45:28 -0300

    Fix on "Record" declaration redundancy.

--- schema-metaclass/src/main/java/org/nanotek/meta/classification/SchemaCrawlerTask.java
+++ schema-metaclass/src/main/java/org/nanotek/meta/classification/SchemaCrawlerTask.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.nanotek.meta.model.rdbms.schema.result.SchemaCrawlerResult;
 
-public interface SchemaCrawlerTask<R extends Record > extends Task<R , SchemaCrawlerResult<?>> {
+public interface SchemaCrawlerTask<R> extends Task<R , SchemaCrawlerResult<?>> {
 
 	@Override
 	default Optional<SchemaCrawlerResult<?>> evaluate(R cd) {
