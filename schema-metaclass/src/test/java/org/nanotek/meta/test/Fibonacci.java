f63cc384fb386343c59e09e85d2da9b6c69e9899
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-01-16 09:50:27 -0300

    algorithm proof print

--- schema-metaclass/src/test/java/org/nanotek/meta/test/Fibonacci.java
+++ schema-metaclass/src/test/java/org/nanotek/meta/test/Fibonacci.java
@@ -1,9 +1,9 @@
 package org.nanotek.meta.test;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.Supplier;
 
 
 /**
@@ -16,13 +16,15 @@ public class Fibonacci {
 	private  long fibo (long x)
 	{
 		var result = memo.get(x);
+		System.out.println("entered fibo x " + x + " result " + result);
 		return Optional.ofNullable(result).orElseGet(() ->  fib(x));
 	}
 	
 	private  long fib(long x)
-	{ 	
-			var result = Optional.ofNullable(x).filter(y -> y >2).map( y -> fib(y-1) + fibo(y-2)).orElse(1L);
+	{ 	    
+			var result = Optional.ofNullable(x).filter(y -> y >2).map( y -> fib(y-1) + fibo(y-2)).orElseGet(() -> x>0?1L:0L);
 			memo.put(x, result);
+			System.out.println("produced fib " + x + " result x " + "  " + result);
 			return result;
 	 	
 	 }
