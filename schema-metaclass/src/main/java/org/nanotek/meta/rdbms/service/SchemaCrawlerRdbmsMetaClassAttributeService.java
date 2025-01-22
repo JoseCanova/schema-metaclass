863446e5c944f97494efcb0a4d61609ab016ad9c
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-01-22 13:25:11 -0300

    renamed class.

--- schema-metaclass/src/main/java/org/nanotek/meta/rdbms/service/SchemaCrawlerRdbmsMetaClassAttributeService.java
+++ schema-metaclass/src/main/java/org/nanotek/meta/rdbms/service/SchemaCrawlerRdbmsMetaClassAttributeService.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import schemacrawler.schema.Column;
 
-public class SchemaCrawlerRdbmsMetaAttributeService {
+public class SchemaCrawlerRdbmsMetaClassAttributeService {
 
 	
 	@Autowired 
